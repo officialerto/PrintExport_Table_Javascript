ed8ac37a768bfc1c0048bbf3d498585f0fbfeca4
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-01-05 20:52:09 +0300

    Add project files.

---
+++ PrintExport_Table_jQuery/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrintExport_Table_jQuery.Models;
+
+namespace PrintExport_Table_jQuery.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+            List<Employee> emplist = new List<Employee>();
+            using (DbModels db = new DbModels())
+            {
+                emplist = db.Employees.ToList<Employee>();
+                return Json(new { data = emplist }, JsonRequestBehavior.AllowGet);
+            }
+            return View();
+        }
+    }
+}
---
+++ PrintExport_Table_jQuery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrintExport_Table_jQuery.MvcApplication" Language="C#" %>
